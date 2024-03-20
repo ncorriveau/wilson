402064ab9b61a7508253a179b362c56abcc701bb
Nicko Corriveau <nc2942@columbia.edu>
2024-03-19 20:52:46 -0400

    adding generalized info extract

--- src/extract_perscriptions_oai.py
+++ src/extract_perscriptions_oai.py
@@ -1,6 +1,7 @@
 import logging
 import sys
-from typing import List
+from dataclasses import dataclass
+from typing import List, Type
 
 from llama_index.core import SimpleDirectoryReader
 from openai import OpenAI
@@ -8,6 +9,36 @@ from pydantic import BaseModel, ConfigDict, Field
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+
+PERSCRIPTION_SYS_MSG = """You will assist in analyzing medical records and returning a list of current medications the patient is taking in a valid JSON format. You just return 
+valid JSON objects that contain the technical name, brand name, and instructions for each drug found."""
+
+PERSCRIPTION_USER_MSG = """Extract the perscribed medications from the doctor note below and return a list of current medications the patient is taking in a valid JSON format. 
+Doctor Note: 
+{}"""
+
+METADATA_SYS_MSG = """return metadata about the particular appointment in a valid JSON format. The JSON objects must contain the doctor's name and the date of the appointment. If you are unsure, please respond with an empty string for each category"""
+
+METADATA_USER_MSG = """Extract the doctor's name and the date of the appointment from the doctors note below and return the information in valid JSON format.
+Doctor Note:
+{}"""
+
+
+def build_system_msg(msg: str, schema: dict[str]) -> str:
+    """helper function to build system messages. The msg should contain more specific instructions about the task,
+    while the schema should match what you want your response JSON schema to look like
+    """
+    return f"""You are an expert AI assistant for medical practitioners. You will assist in analyzing medical records, extracting information, and returning the information in valid JSON format. Specifically, please complete the following:
+            {msg}. Follow the JSON schema provided below to ensure the information is returned in the correct format.
+            JSON Schema: 
+            {schema}"""
+
+
+@dataclass
+class Messages:
+    system: str = ""
+    user: str = ""
+    assistant: str = "```json"
 
 
 class Drug(BaseModel):
@@ -58,56 +89,50 @@ class AppointmentMeta(BaseModel):
 
     model_config = ConfigDict(
         json_schema_extra={
-            {
-                "dr_name": "Dr. Oz",
-                "date": "2023-10-10",
-            },
-        }
+            "dr_name": "Dr. Oz",
+            "date": "2023-10-10",
+        },
     )
 
 
-SYSTEM_MESSAGE = f"""You are an expert AI assistant for medical practitioners. You will assist in analyzing 
-medical records and returning a list of current medications the patient is taking in a valid JSON format. You just return 
-valid JSON objects that contain the technical name, brand name, and instructions for each drug found that follows
-the schema below:
-
-JSON Schema: 
-{Perscriptions.model_json_schema()}
-"""
-USER_MESSAGE = """Extract the perscribed medications from the doctor note below and return a list of current medications the patient is taking in a valid JSON format. 
-Doctor Note: 
-{}"""
-ASSISTANT_MESSAGE = "```json"
-
-
-def extract_perscriptions(client: OpenAI, note: str) -> Perscriptions:
+def extract_info(
+    client: OpenAI, messages: Messages, model: Type[BaseModel]
+) -> Type[BaseModel]:
+    """Extracts information from a given document using the OpenAI API."""
     response = client.chat.completions.create(
-        model="gpt-4",
+        model="gpt-4-1106-preview",
         max_tokens=1000,
         temperature=0.1,
         stop=["```"],
         messages=[
-            {"role": "system", "content": SYSTEM_MESSAGE},
-            {"role": "user", "content": USER_MESSAGE.format(note)},
-            {"role": "assistant", "content": ASSISTANT_MESSAGE},
+            {"role": "system", "content": messages.system},
+            {"role": "user", "content": messages.user},
+            {"role": "assistant", "content": messages.assistant},
         ],
-        response_format="json_object",
+        response_format={"type": "json_object"},
     )
-    return Perscriptions.model_validate_json(response.choices[0].message.content)
+    return model.model_validate_json(response.choices[0].message.content)
 
 
 if __name__ == "__main__":
     client = OpenAI()
     document = SimpleDirectoryReader("data").load_data()
-    response = extract_perscriptions(client, document)
+    messages = Messages(
+        system=build_system_msg(METADATA_SYS_MSG, AppointmentMeta.model_json_schema()),
+        user=METADATA_USER_MSG.format(document),
+    )
+    response = extract_info(client, messages, AppointmentMeta)
+    print(response)
+    # schema = Perscriptions.model_json_schema()
+    # response = extract_perscriptions(client, document)
 
-    TRUE_NAMES = {"flonase", "nexium", "naprosyn", "astelin", "desyrel"}
-    TEST_NAMES = set()
+    # TRUE_NAMES = {"flonase", "nexium", "naprosyn", "astelin", "desyrel"}
+    # TEST_NAMES = set()
 
-    for drug in response.drugs:
-        print(f"Checking {drug.brand_name.lower()}")
-        assert drug.brand_name.lower() in TRUE_NAMES
-        TEST_NAMES.add(drug.brand_name.lower())
+    # for drug in response.drugs:
+    #     print(f"Checking {drug.brand_name.lower()}")
+    #     assert drug.brand_name.lower() in TRUE_NAMES
+    #     TEST_NAMES.add(drug.brand_name.lower())
 
-    assert TEST_NAMES == TRUE_NAMES
-    print("All tests passed!")
+    # assert TEST_NAMES == TRUE_NAMES
+    # print("All tests passed!")
