79abb01024240965d68ef6a81006c2b75b2c4ee3
Nicko Corriveau <nc2942@columbia.edu>
2024-05-16 20:05:33 -0400

    add global themeing

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -1,22 +1,12 @@
 import React from "react";
-import { extendTheme } from '@chakra-ui/react'
 import { ChakraProvider } from '@chakra-ui/react'
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-// import "./index.css";
-
-const colors = {
-  brand: {
-    900: '#1a365d',
-    800: '#153e75',
-    700: '#2a69ac',
-  },
-}
-const theme = extendTheme({ colors })
+import customTheme from "./theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
+    <ChakraProvider theme={customTheme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
---
+++ src/frontend/src/theme.ts
@@ -0,0 +1,42 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const customTheme = extendTheme({
+  colors: {
+    green: {
+      800: "#2F855A",
+      700: "#38A169",
+    },
+  },
+  components: {
+    Button: {
+      baseStyle: {
+        fontWeight: "bold",
+        color: "white",
+      },
+      sizes: {
+        lg: {
+          h: "48px",
+          fontSize: "lg",
+          px: "32px",
+        },
+      },
+      variants: {
+        solid: (props: any) => ({
+          bg: props.colorScheme === "green" ? "green.800" : undefined,
+          color: "white",
+          _hover: {
+            bg: props.colorScheme === "green" ? "green.700" : undefined,
+          },
+        }),
+      },
+    },
+    Heading: {
+      baseStyle: {
+        fontFamily: "serif",
+        color: "green.800",
+      },
+    },
+  },
+});
+
+export default customTheme;
