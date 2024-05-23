d0df935bc7268a2cb53dc23fd40e7f7b9b1a6752
Nicko Corriveau <nc2942@columbia.edu>
2024-05-22 20:17:18 -0400

    adding routes to app

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
 import AppointmentManager from "./components/Appointments/Appointments";
 import ChatApp from "./components/Chat/ChatApp";
 import LoginPage from "./components/Auth/Login";
 import Prescriptions from "./components/Prescriptions/Prescriptions";
+import './index.css';
 
 const App = () => {
   const [token, setToken] = useState<string | null>(null);
@@ -30,14 +32,41 @@ const App = () => {
     return <div>Loading...</div>;
   }
 
-  if (!token || !userId) {
-    return <LoginPage setToken={handleSetToken} />;
-  }
   return (
-    <div className="App">
-      <ChatApp token={token} userId={userId} />
-    </div>
-  );
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/appointments">Appointments 🩺 </Link>
+            </li>
+            <li>
+              <Link to="/chat">Chat with your data! 🤖 </Link>
+            </li>
+            <li>
+              <Link to="/prescriptions">Prescriptions 💊 </Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+        
+        <Routes>
+        {token && userId ? (
+          <>
+            <Route path="/appointments" element={<AppointmentManager token={token} userId={userId} />} />
+            <Route path="/chat" element={<ChatApp token={token} userId={userId} />} />
+            <Route path="/prescriptions" element={<Prescriptions token={token} userId={userId} />} />
+          </>
+        ) : (
+          <Navigate to="/login" />
+        )}
+        <Route path="/login" element={<LoginPage setToken={handleSetToken} />} />
+      </Routes>
+      </div>
+    </Router>
+  )
 };
 
 export default App;
